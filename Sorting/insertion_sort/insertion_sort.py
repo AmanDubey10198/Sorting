c908133047b86f4e54627ab1d569bf231dd8d7ae
Aman dubey <35611660+AmanDubey10198@users.noreply.github.com>
2019-04-05 15:01:06 +0530

    Update insertion_sort.py
    Basic insertion sort algorithm, performs better than other quadratic time taking algorithms because it goes through only single pass if the array is already sorted.

--- Sorting/insertion_sort/insertion_sort.py
+++ Sorting/insertion_sort/insertion_sort.py
@@ -5,8 +5,6 @@ Created on Mon Jun  4 23:15:47 2018
 @author: Aman Dubey
 """
 
-x = [2,4,5,6,1,3]
-
 #Insertion sort
 def insertion_sort(y):
     for i in range(1,len(y)):
@@ -14,8 +12,9 @@ def insertion_sort(y):
         key = y[i]
         j = i-1
     #compare key from previous values
-        while j>-1:
-            if key < y[j]:
+        while j>-1: # iterate the array index reach -1 or equal or smaller element than key is found
+            
+            if key < y[j]: # if key is better smaller than the element at curent index than sort
                 y[j+1] = y[j]
                 j -=1
             else:
@@ -23,6 +22,7 @@ def insertion_sort(y):
         y[j + 1] = key
     return y
 
+
+x = [2,4,5,6,1,3]
 insertion_sort(x)
 print(x)
-
